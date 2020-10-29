4ef6bf34b62d51d0855067829c139579afe5628b
lilkrohn <lilykrohn@gmail.com>
2020-10-29 18:10:59 -0500

    added some intial files

---
+++ app/src/main/java/com/example/adoptme/SwipeScreen.java
@@ -0,0 +1,14 @@
+package com.example.adoptme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SwipeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.swipe_screen);
+    }
+}
