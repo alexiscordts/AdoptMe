0349b0494a0cdc94ee6d7751ad5ac50b721146a6
lilkrohn <lilykrohn@gmail.com>
2020-10-29 18:43:18 -0500

    adding more files and layout changes

---
+++ app/src/main/java/com/example/adoptme/LikedPets.java
@@ -0,0 +1,14 @@
+package com.example.adoptme;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LikedPets extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.liked_pets);
+    }
+}
